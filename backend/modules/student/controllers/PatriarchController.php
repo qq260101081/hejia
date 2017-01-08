14824f30bf087b59c129d680ef1f59ddb8aa4456
carl <260101081@qq.com>
2017-01-08 21:53:58 +0800

    调试

--- backend/modules/student/controllers/PatriarchController.php
+++ backend/modules/student/controllers/PatriarchController.php
@@ -105,7 +105,6 @@ class PatriarchController extends CommonController
         }
         else
         {
-            var_dump($model->getErrors());die;
             Yii::$app->session->setFlash('error', ['delay'=>3000,'message'=>'开通失败！']);
         }
         return $this->redirect(['index']);
