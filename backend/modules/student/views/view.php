6bf999acc02929bfac1f9232c727441d6580e462
carl <260101081@qq.com>
2017-01-09 16:08:57 +0800

    查看学生页面恢复

---
+++ backend/modules/student/views/view.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\guarantee\models\Guarantee */
+
+$this->title = Yii::t('app', 'View');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Student List'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="guarantee-view">
+
+    <h1><?= Html::encode($model->name) ?></h1>
+
+    <p>
+        <?php if(Yii::$app->user->can('student/student/update')) echo Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?php if(Yii::$app->user->can('student/student/delete')) echo Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'name',
+            'sex',
+            'age',
+            'school',
+            'grade',
+            'created_at:date',
+            'updated_at:date',
+        ],
+    ]) ?>
+
+</div>
