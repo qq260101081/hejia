0002c640bdf47ae371b0f1c17a995da306c1cd0a
carl <260101081@qq.com>
2017-01-10 12:59:23 +0800

    前端修正

--- api/views/user/password.php
+++ api/views/user/password.php
@@ -15,7 +15,7 @@
 
 <?php if(!Yii::$app->getSession()->getFlash('success')):;?>
 <?php $form = ActiveForm::begin();?>
-    <input type="text" name="mobile" value="<?=$model->username;?>" class="login-password">
+    <input type="text" name="mobile" value="<?=$model->username;?>" disabled class="login-password">
     <!--<div class="yzm">
         <input type="text" class="reg-yzm" placeholder="输入验证码">
         <input type="button" class="yzm-hq" value="获取验证码">
